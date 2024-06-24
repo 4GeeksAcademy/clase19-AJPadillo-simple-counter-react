705891d8ad14d047aab0eb32c154ab5db3232bb8
AJPadillo <98108563+AJPadillo@users.noreply.github.com>
2024-06-24 12:24:50 +0000

    pendiente icono font awesome

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,38 @@
 import React from "react";
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 import { SecondsCounter } from "./secondsCounter";
 
 //create your first component
 const Home = () => {
-	const [segundos, setSegundos] = useState(0)
+	const [segundos, setSegundos] = useState(0);
+	const [decimales, setDecimales] = useState(0);
+	const [centesimales, setCentesimales] = useState(0);
 
-	setInterval(() => {
-		setSegundos(segundos + 1);
-	}, 1000);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setSegundos(prevSegundos => {
+				if (prevSegundos >= 9) {
+					setDecimales(prevDecimales => {
+						if (prevDecimales >= 9) {
+							setCentesimales(prevCentesimales => prevCentesimales + 1);
+							return 0;
+						} else {
+							return prevDecimales + 1;
+						}
+					});
+					return 0;
+				} else {
+					return prevSegundos + 1;
+				}
+			});
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
-		<SecondsCounter seconds={segundos} />
+		<SecondsCounter seconds={segundos} decimals={decimales} centesimals={centesimales} />
 	);
 };
 
--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 export const SecondsCounter = (props) => {
+    let { seconds, decimals, centesimals } = props;
+
     return (
         <>
             <h1>
-                {props.seconds}
+                {}
             </h1>
             <div className="container">
-                <div className="row d-flex bg-dark gap-2">
-                    <span className="col bg-secondary">{props.seconds}</span>
-                    <span className="col bg-secondary">{props.seconds}</span>
-                    <span className="col bg-secondary">{props.seconds}</span>
-                    <span className="col bg-secondary">{props.seconds}</span>
-                    <span className="col bg-secondary">{props.seconds}</span>
+                <div className="row d-flex gap-1 bg-dark text-center">
+                    <h1 className="col-md m-3 ms-5 bg-secondary rounded text-white"><i className="fa-solid fa-house"></i></h1>
+                    <h1 className="col-md m-3 bg-secondary rounded text-white">0</h1>
+                    <h1 className="col-md m-3 bg-secondary rounded text-white">0</h1>
+                    <h1 className="col-md m-3 bg-secondary rounded text-white">{centesimals}</h1>
+                    <h1 className="col-md m-3 bg-secondary rounded text-white">{decimals}</h1>
+                    <h1 className="col-md m-3 me-5 bg-secondary rounded text-white">{seconds}</h1>
                 </div>
             </div>
         </>
